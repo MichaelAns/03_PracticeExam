42eaff1ff529f33fccc15ffd16072091a0f2bf17
Михаил Щукин <mi035ms@gmail.com>
2023-04-19 08:50:07 +0300

    Created the class Car

---
+++ src/ThePracticeExam.Program/Classes/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace ThePracticeExam.Program.Classes
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public double Power { get; set; }
+
+        public override string ToString()
+        {            
+            return $"{Model} {Price} {Power}";
+        }
+    }
+}
